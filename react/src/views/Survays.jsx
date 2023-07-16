c572770f694df78072305cba0e162ea3c0c06939
MohamedEssam15 <mesam5081@gmail.com>
2023-07-16 18:32:14 +0300

    set axios

--- react/src/views/Survays.jsx
+++ react/src/views/Survays.jsx
@@ -1,5 +1,7 @@
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import PageComponent from "../components/PageComponent";
 import SurvayListItem from "../components/SurvayListItem";
+import TButton from "../components/core/Tbutton";
 import { useStateContext } from "../contexts/ContextProvider";
 
 
@@ -11,7 +13,10 @@ console.log(Survays);
         console.log('on Delete Link')
     }
     return(
-      <PageComponent title="Survays">
+      <PageComponent title="Survays" buttons={(<TButton color="green" to="/survays/create">
+        <PlusCircleIcon className="h-6 w-6 mr-2"/>
+        Create new
+      </TButton>)}>
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
 
             {Survays.map((survey)=>(
