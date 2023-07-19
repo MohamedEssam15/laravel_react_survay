86ecf24c2555228095720486948585ceeb75151a
MohamedEssam15 <mesam5081@gmail.com>
2023-07-20 01:10:15 +0300

    survey backend

---
+++ database/migrations/2023_07_19_155225_create_survey_questions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('survey_questions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('survey_id')->unsigned();
+            $table->foreign('survey_id')->references('id')->on('surveys')->onDelete('cascade');
+            $table->string('type',45);
+            $table->string('question',2000);
+            $table->longText('description')->nullable();
+            $table->longText('data')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('survey_questions');
+    }
+};
